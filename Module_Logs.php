9c1719f9e92cb3150e76d7d1804db25633cf7fa2
gizmore <gizmore@wechall.net>
2017-09-13 07:04:16 +0200

    sync

--- Module_Logs.php
+++ Module_Logs.php
@@ -4,13 +4,14 @@ namespace GDO\Logs;
 use GDO\Core\GDO_Module;
 use GDO\Date\GDT_Date;
 use GDO\Type\GDT_Checkbox;
+use GDO\Core\Logger;
 
 final class Module_Logs extends GDO_Module
 {
     public function getConfig()
 	{
 		return array(
-			GDT_Checkbox::make('log_requests')->initial('0'),
+			GDT_Checkbox::make('log_requests')->initial('1'),
 			GDT_Checkbox::make('log_rotation_mail')->initial('1'),
 			GDT_Checkbox::make('log_rotation')->initial('1'),
 			GDT_Date::make('last_log_rotation'),
@@ -25,4 +26,12 @@ final class Module_Logs extends GDO_Module
 	public function renderAdminTabs() { return $this->templatePHP('admin_tabs.php'); }
 	
 	public function href_administrate_module() { return href('Logs', 'Overview'); }
+	
+	public function onInit()
+	{
+	    if ($this->cfgLogRequests())
+	    {
+	        Logger::log('requests', json_encode($_REQUEST));
+	    }
+	}
 }
