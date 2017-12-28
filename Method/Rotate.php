f95d17afbcf7e12665d2f3a32b96307f1c659774
gizmore <gizmore@wechall.net>
2017-12-28 15:33:10 +0100

    sync

--- Method/Rotate.php
+++ Method/Rotate.php
@@ -4,6 +4,9 @@ namespace GDO\Logs\Method;
 use GDO\Core\MethodCronjob;
 use GDO\Date\Time;
 use GDO\Logs\Module_Logs;
+use GDO\File\GDO_File;
+use GDO\File\FileUtil;
+use GDO\Mail\Mail;
 
 final class Rotate extends MethodCronjob
 {
@@ -28,5 +31,22 @@ final class Rotate extends MethodCronjob
 	{
 		$this->logNotice('Log Rotation');
 		
+		FileUtil::createDir(GWF_PATH . 'protected/logs_zipped');
+		
+		$filename = sitename() . '_' . date('Ymd') . '.log.zip';
+		$command = "cd protected && find logs -daystart -mtime +1 | zip -r9 logs_zipped/$filename -@";
+		exec($command, $output, $return_val);
+		echo print_r($output, 1);
+		
+		if (Module_Logs::instance()->cfgLogRotationMail())
+		{
+			$this->sendLogMails($filename);
+		}
+	}
+	
+	private function sendLogMails($filename)
+	{
+		$mail = Mail::botMail();
+		$mail->addAttachmentFile($filename, 'logs_zipped'.$filename)
 	}
 }
