082583ff20cfc35354a18b95f22eaddbbfe7b8fc
gizmore <gizmore@wechall.net>
2018-09-09 02:03:45 +0200

    6.08-r38_install_fixes

--- Module_Logs.php
+++ Module_Logs.php
@@ -6,6 +6,14 @@ use GDO\Date\GDT_Date;
 use GDO\DB\GDT_Checkbox;
 use GDO\Core\Logger;
 
+/**
+ * This module offers logging of requests and log rotation.
+ * The zipped logs can be sent to all admins regularly.
+ * 
+ * @author gizmore@wechall.net
+ * @version 6.08
+ * @since 5.00
+ */
 final class Module_Logs extends GDO_Module
 {
 	public function getConfig()
@@ -17,7 +25,6 @@ final class Module_Logs extends GDO_Module
 			GDT_Date::make('last_log_rotation'),
 		);
 	}
-	
 	public function cfgLogRequests() { return $this->getConfigValue('log_requests'); }
 	public function cfgLogRotationMail() { return $this->getConfigValue('log_rotation_mail'); }
 	public function cfgLogRotation() { return $this->getConfigValue('log_rotation'); }
@@ -26,6 +33,8 @@ final class Module_Logs extends GDO_Module
 	public function renderAdminTabs() { return $this->templatePHP('admin_tabs.php'); }
 	
 	public function href_administrate_module() { return href('Logs', 'Overview'); }
+	
+	public function onLoadLanguage() { return $this->loadLanguage('lang/logs'); }
 	
 	public function onInit()
 	{
